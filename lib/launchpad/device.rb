7ddeae1448f0c1a7dff4d0fbf4655b45462c0944
Mo Morsi <mmorsi@redhat.com>
2018-01-21 09:05:16 -0500

    Use regex for device name to match all Launchpad devices

--- lib/launchpad/device.rb
+++ lib/launchpad/device.rb
@@ -338,8 +338,8 @@ module Launchpad
       logger.debug "creating #{device_type} with #{opts.inspect}, choosing from portmidi devices #{devices.inspect}"
       id = opts[:id]
       if id.nil?
-        name = opts[:name] || 'Launchpad'
-        device = devices.select {|device| device.name == name}.first
+        name = opts[:name] || /Launchpad.*/
+        device = devices.select {|device| device.name =~ name}.first
         id = device.device_id unless device.nil?
       end
       if id.nil?
